b9161c0e0c508100f048d4dc1e40790823bbd127
Jon Mentzell <jmentzell@redhat.com>
2022-10-17 14:21:15 -0400

    Reorganized.  Cleaned up.  Example crontab

--- pingfunctions.sh
+++ pingfunctions.sh
@@ -9,8 +9,4 @@ function startup () {
 	if [ -d $OUTDIR ] && [ -L $OUTDIR ] 
 	echo Output directory not detected.  Creating ${OUTDIR}...
 	mkdir -p $OUTDIR
-
-function cleanup () {
-	cat /dev/null > $INTFILE
-	cat /dev/null > $EXTFILE
 }
--- pingmonster.sh
+++ pingmonster.sh
@@ -3,10 +3,12 @@
 source ./pingvars.sh
 source ./pingfunctions.sh
 
+startup()
 
 # Internal - check the firewall is reachable:
-$PING $PINGFLAGS $PINGINT | grep rtt > /tmp/int.txt
+$PING $PINGFLAGS $PINGINT | grep rtt | awk '{print $4}' | sed -e 's/\//,/g' > $INTCSV
 
 # External - get stats
-$PING $PINGFLAGS $PINGEXT | grep rtt > /tmp/ext.txt
+$PING $PINGFLAGS $PINGEXT | grep rtt | awk '{print $4}' | sed -e 's/\//,/g' > $EXTCSV
 
+# cleanup()
