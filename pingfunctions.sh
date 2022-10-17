847fe651628c764038c43dfcb86d01c495b1bf07
Jon Mentzell <jmentzell@redhat.com>
2022-10-17 14:05:47 -0400

    adding vars to function

--- pingfunctions.sh
+++ pingfunctions.sh
@@ -2,6 +2,9 @@
 
 # Externalize functions for pingmonster.
 
+# in theory the next line shouldn't be necessary:
+source ./pingvars.sh
+
 function startup () {
 	if [ -d $OUTDIR ] && [ -L $OUTDIR ] 
 	echo Output directory not detected.  Creating ${OUTDIR}...
